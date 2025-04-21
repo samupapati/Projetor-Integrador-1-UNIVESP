27eafadc99b86825fd594752775ddbef02c112b7
Samuel <samuel@debian.debian>
2025-04-21 17:27:46 -0300

    Implementação de login

--- API/API.py
+++ API/API.py
@@ -10,6 +10,8 @@ conexaoBD = psycopg2.connect(database = "estoqueBD",
 cursor = conexaoBD.cursor()
 app = Flask(__name__)
 
+#Tabela produtos
+
 # Criar
 @app.route('/produtos', methods=["POST"])
 def criarProduto():
@@ -57,5 +59,49 @@ def excluirProduto(cod):
     cursor.execute(f"DELETE FROM produtos WHERE cod = {cod}")
     conexaoBD.commit()
     return "Operação completa"
+
+
+#Tabela usuarios
+
+def verificaUsuario(nome):
+    cursor.execute(f"""
+                        SELECT COUNT(*) AS COUNT FROM usuarios WHERE usr_nome = '{nome}' 
+                   """)
+    data = cursor.fetchall()
+
+    return data[0][0] # Sem os zeros retornará dessa forma: [()] Primeiro zero: acessa a primeira tupla, segundo zero acessa o número
+
+@app.route('/usuario', methods=['POST'])
+def criarUsuario():
+    nome = request.get_json()["nome"]
+    senha = request.get_json()["senha"]
+    
+    if verificaUsuario(nome) == 1:
+        return "Usuário já criado."
+    else:
+        cursor.execute(f"""
+                            INSERT INTO usuarios(usr_nome, usr_senha) VALUES ('{nome}', '{senha}');
+                       """)
+        conexaoBD.commit()
+        return "Usuário criado com sucesso."
+
+@app.route('/usuario/login', methods=['POST'])
+def login():
+    nome = request.get_json()["nome"]
+    senha = request.get_json()["senha"]
+    
+    if verificaUsuario(nome) == 1:
+        cursor.execute(f"""
+                            SELECT usr_senha FROM usuarios WHERE usr_nome = '{nome}' 
+                       """)
+        data = cursor.fetchall()[0][0]
+        
+        if data == senha:
+            return "Logado."
+        else:
+            return "Senha incorreta"
+        
+    else:
+        return "Usuário não encontrado."
     
 app.run(port=5000, host='localhost', debug=True)
